6eb282f500dbffd9292336056a59c6eeb9dcb4f4
smjxpro <smjxpro@gmail.com>
2019-04-11 08:05:30 +0600

    model

---
+++ SimpleCalculator/CalculatorEngine.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    internal class CalculatorEngine
+    {
+        internal double Calculate(string operation, double firstNumber, double secondNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SimpleCalculator/InputConverter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    internal class InputConverter
+    {
+        internal double ConvertInputToNumeric(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
